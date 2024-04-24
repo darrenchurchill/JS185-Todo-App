e6778b0a32dc0bdccd2d2ee98b7b889d3019f03f
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-23 17:46:45 -0700

    6.2: Add app skeleton

---
+++ todos.js
@@ -0,0 +1,25 @@
+/**
+ * JS175 Todo App
+ * todos.js
+ */
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const HOST = "localhost";
+const PORT = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
+
+app.get("/", (_req, res) => {
+  res.render("lists");
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Todos server listening on ${HOST}:${PORT}...`);
+});
