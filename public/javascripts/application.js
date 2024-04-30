f755f4a92869542659643517ac510674cb62d2e8
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-30 11:26:36 -0700

    6.16: Add client-side confirmation dialogs for "dangerous" ops

---
+++ public/javascripts/application.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let forms = document.querySelectorAll("form.delete, form.complete_all");
+  forms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (confirm("Are you sure? This can't be undone!")) {
+        event.target.submit();
+      }
+    });
+  });
+});
