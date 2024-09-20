02d3bd3f1429dd2ae3dd6344e6618ce72dcf0e73
Darren Churchill <darrenmchurchill@gmail.com>
2024-09-19 18:06:37 -0700

    Add test query to view a single list w/ its metadata

--- sql/queries.sql
+++ sql/queries.sql
@@ -29,3 +29,44 @@ ORDER BY
   done,
   lower(title)
 ;
+
+-- @block
+-- @conn todo-lists
+-- @label view a single todolist and metadata sorted by todo done-ness and title
+WITH
+  list_metadata AS (
+    SELECT
+      *,
+      "length" > 0
+      AND "countDone" = "length" AS done
+    FROM
+      (
+        SELECT
+          tl.id "listID",
+          tl.title,
+          count(t.id)::integer "length",
+          coalesce(sum(t.done::integer), 0)::integer "countDone"
+        FROM
+          todolists tl
+          LEFT JOIN todos t ON tl.id = t.todolist_id
+        GROUP BY
+          tl.id
+        HAVING
+          tl.id = 3
+      )
+  )
+SELECT
+  t.id,
+  t.title,
+  t.done,
+  lm."listID",
+  lm.title "listTitle",
+  lm.length "listLength",
+  lm."countDone"
+FROM
+  todos t
+  RIGHT JOIN list_metadata lm ON t.todolist_id = lm."listID"
+ORDER BY
+  t.done,
+  lower(t.title)
+;
