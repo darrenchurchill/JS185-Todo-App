140aae5e7b893c82cb1af45706cdf122d73a80b3
Darren Churchill <darrenmchurchill@gmail.com>
2024-10-16 12:45:12 -0700

    Add transaction methods and option to throw query-related errors
    I'd have probably preferred to commit this as 2 or more separate
    changes, but I don't want to go back and pull it apart now that it's
    written here.
    
    Since there are a variety of reasons why a database-querying method
    might fail, if the caller cares why a method failed, they should inspect
    the thrown Error. If they don't care why a method failed, they can just
    inspect the return value.
    
    Using transactions makes it easier to separate route param and form
    param validation procedures from the remaining middleware that expects
    those values to be valid. You don't have to worry about other concurrent
    database processes altering data between its validation and its final
    use.

--- todos.js
+++ todos.js
@@ -28,18 +28,20 @@ const {
   validationResult,
 } = new ExpressValidator(
   {
-    attemptAddList: attempt(
-      (title, { req }) => req.res.locals.todoStore.addList(title),
-      "You're already using that list title. List titles must be unique."
-    ),
+    isUniqueListTitle: async (title, { req }) => {
+      if (req.res.locals.todoList && title === req.res.locals.todoList.title) {
+        // Allow "updating" a list's title to its current title, since
+        // TodoPGStore.setListTitle() allows this.
+        return true;
+      }
 
-    attemptSetListTitle: attempt(
-      (title, { req }) => req.res.locals.todoStore.setListTitle(
-        matchedData(req).listID,
-        title
-      ),
-      "You're already using that list title. List titles must be unique."
-    ),
+      if (await req.res.locals.todoStore.listTitleExists(title)) {
+        throw new Error(
+          "You're already using that list title. List titles must be unique."
+        );
+      }
+      return true;
+    },
   },
   {},
   {
@@ -48,33 +50,60 @@ const {
 );
 
 /**
- * Create a new custom validator function that, when invoked, attempts to
- * execute the provided `callback`, using the given `errMsg` if the callback
- * fails.
- * @param {function} callback the callback to attempt, following the
- * `express-validator` custom validator signature: return a boolean indicating
- * validity/success and optionally throw an Error given an invalid value
- * @param {string} errMsg the error message to use if the callback fails
- * @returns {true} return `true` if callback succeeds
- * @throws {Error} throw an Error if the callback fails
+ * Given an Error, return a new Error, wrapping the original Error, using
+ * `err.cause.code` and the set of `expectedErrCodes` to determine the new
+ * Error's `message`. Return the original `err` if it doesn't have a `cause` or
+ * if its `cause.code` isn't in `expectedErrCodes`'s set of codes.
+ * @param {Error} err the given Error
+ * @param {Object.<string, string>} expectedErrCodes an object mapping expected
+ * string error `code`s to string error messages to use in the wrapped Error
+ * result.
+ * @returns {Error} the new wrapped Error, with the original Error as its
+ * `cause`, or the original `err` if it represents an unexpected Error.
  */
-function attempt(callback, errMsg) {
-  return async function(value, { req, location, path, pathValues }) {
+function wrapIfExpected(err, expectedErrCodes = {}) {
+  if (err.cause && Object.keys(expectedErrCodes).includes(err.cause.code)) {
+    return new Error(expectedErrCodes[err.cause.code], { cause: err });
+  }
+  return err;
+}
+
+/**
+ * Return a new middleware function that, when invoked, attempts to execute the
+ * provided `callback`, catching any thrown Errors and passing them to the
+ * `next` function to be handled. `callback()` is expected to invoke `next()`
+ * within its body if there is more middleware to execute.
+ *
+ * This function is intended for use in express prior to v5.*, where
+ * asynchronous middleware needs to be explicitly passed to `next()`.
+ * @param {function} callback the callback to attempt, following the standard
+ * express middleware signature
+ * @param {object} options the set of additional options
+ * @param {object.<string, string>} expectedErrCodes the set of string Error
+ * code keys and new Error message string values to wrap any expected Error with
+ * @param {boolean} flashExpectedErrs whether to flash any expected Error
+ * message to the application user
+ * @returns {function} the new wrapped middleware function
+ */
+// eslint-disable-next-line max-lines-per-function
+function withAttemptAsync(
+  callback,
+  options = {
+    expectedErrCodes: {},
+    flashExpectedErrs: false,
+  }
+) {
+  return async function (req, res, next) {
     try {
-      if (await callback(value, { req, location, path, pathValues })) {
-        return true;
-      }
+      await callback(req, res, next);
     } catch (err) {
-      const wrapped = new Error(
-        "Unable to execute this operation. Contact admin if this problem persists.",
-        {
-          cause: err,
-        }
-      );
-      console.log(wrapped);
-      throw wrapped;
+      const newErr = wrapIfExpected(err, options.expectedErrCodes);
+      const isExpected = err !== newErr; // the Error has been wrapped
+      if (isExpected && options.flashExpectedErrs) {
+        req.flash("error", newErr.message);
+      }
+      next(newErr);
     }
-    throw new Error(errMsg);
   };
 }
 
@@ -115,34 +144,16 @@ function createFormValidationChain(fieldName, fieldDesc) {
     .withMessage(`Max ${fieldDesc} length is 100 characters.`);
 }
 
-function createPathParamValidationChain(paramName, paramDesc, finalCallback) {
+function createPathParamValidationChain(paramName, paramDesc) {
   return param(paramName)
     .isInt()
     .withMessage(`That isn't a ${paramDesc} ID; ${paramDesc} IDs are integers.`)
     .bail()
-    .toInt()
-    .custom(async (value, { req, location, path, pathValues }) => {
-      let isValid = false;
-      try {
-        isValid = await finalCallback(value, {
-          req,
-          location,
-          path,
-          pathValues,
-        });
-      } catch (err) {
-        console.log(err);
-        throw new Error(
-          `That ${paramDesc} might not exist. ` +
-          "Contact administrator if this problem persists."
-        );
-      }
-
-      if (isValid) return true;
-      throw new Error(`That ${paramDesc} doesn't exist.`);
-    });
+    .toInt();
 }
 
+// TODO: combine this function with attachRequestValidationResult() and
+// flashValidationErrors() below?
 function ifInvalid(callback) {
   return function(req, res, next) {
     if (req.validationResult.isEmpty()) {
@@ -153,12 +164,18 @@ function ifInvalid(callback) {
   };
 }
 
-function attachRequestValidationResult(req, _res, next = () => {}) {
-  req.validationResult = validationResult(req);
-  next();
+function attachRequestValidationResult(
+  options = { throw: false, flashErrs: false }
+) {
+  return function (req, _res, next = () => {}) {
+    req.validationResult = validationResult(req);
+    if (options.flashErrs) flashValidationErrors(req);
+    if (options.throw) req.validationResult.throw();
+    next();
+  };
 }
 
-function flashValidationErrors(req, _res, next) {
+function flashValidationErrors(req, _res, next = () => {}) {
   let result = req.validationResult || validationResult(req);
   result.array().forEach((errMsg) => req.flash("error", errMsg));
   next();
@@ -172,28 +189,24 @@ function flashValidationErrors(req, _res, next) {
  * `TodoList`.
  */
 const lists = {
-  async displayLists(_req, res, next) {
-    try {
-      res.render("lists", {
-        todoLists: await res.locals.todoStore.sortedTodoLists(),
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
+  displayLists: withAttemptAsync(async (_req, res) => {
+    res.render("lists", {
+      todoLists: await res.locals.todoStore.sortedTodoLists(),
+    });
+  }),
 
   get newList() {
     return [
       createFormValidationChain("todoListTitle", "List Title")
-        .attemptAddList(),
-      attachRequestValidationResult,
-      flashValidationErrors,
+        .isUniqueListTitle(),
+      attachRequestValidationResult({ flashErrs: true }),
       ifInvalid(this.reRenderNewListForm),
-      (req, res) => {
-        const title = matchedData(req).todoListTitle;
+      withAttemptAsync(async (req, res) => {
+        const { todoListTitle: title } = matchedData(req);
+        await res.locals.todoStore.addList(title, { throw: true });
         req.flash("success", `Todo List created: "${title}"`);
         res.redirect("/lists");
-      }
+      }),
     ];
   },
 
@@ -214,17 +227,13 @@ const lists = {
  */
 /* eslint-disable max-lines-per-function */
 const list = {
-  async completeAll(req, res, next) {
-    try {
-      const data = matchedData(req);
-      if (await res.locals.todoStore.markAllDone(data.listID)) {
-        req.flash("success", "All todos marked completed.");
-      }
-      res.redirect(`/lists/${data.listID}`);
-    } catch (err) {
-      next(err);
+  completeAll: withAttemptAsync(async (req, res) => {
+    const { listID } = matchedData(req);
+    if (await res.locals.todoStore.markAllDone(listID)) {
+      req.flash("success", "All todos marked completed.");
     }
-  },
+    res.redirect(`/lists/${listID}`);
+  }),
 
   get editListForm() {
     return this.renderEditListForm;
@@ -233,15 +242,15 @@ const list = {
   get editList() {
     return [
       createFormValidationChain("todoListTitle", "List Title")
-        .attemptSetListTitle(),
-      attachRequestValidationResult,
-      flashValidationErrors,
+        .isUniqueListTitle(),
+      attachRequestValidationResult({ flashErrs: true }),
       ifInvalid(this.reRenderEditListForm),
-      (req, res) => {
-        const data = matchedData(req);
+      withAttemptAsync(async (req, res) => {
+        const { todoListTitle: title, listID } = matchedData(req);
+        await res.locals.todoStore.setListTitle(listID, title, { throw: true });
         req.flash("success", "Todo List title updated.");
-        res.redirect(`/lists/${data.listID}`);
-      },
+        res.redirect(`/lists/${listID}`);
+      }),
     ];
   },
 
@@ -256,24 +265,19 @@ const list = {
         next();
       },
 
-      (_req, res, next) => {
-        try {
-          res.render("list", {
-            todoList: res.locals.todoList,
-            todoTitle: res.locals.todoTitle,
-          });
-        } catch (err) {
-          next(err);
-        }
-      }
+      (_req, res) => {
+        res.render("list", {
+          todoList: res.locals.todoList,
+          todoTitle: res.locals.todoTitle,
+        });
+      },
     ];
   },
 
   get newTodo() {
     return [
       createFormValidationChain("todoTitle", "Todo Title"),
-      attachRequestValidationResult,
-      flashValidationErrors,
+      attachRequestValidationResult({ flashErrs: true }),
 
       ifInvalid((req, res) => {
         req.session.todoTitle = req.body.todoTitle;
@@ -281,36 +285,32 @@ const list = {
         res.redirect(`/lists/${data.listID}`);
       }),
 
-      async (req, res, next) => {
-        try {
-          const data = matchedData(req);
-          await res.locals.todoStore.addTodo(data.listID, data.todoTitle);
-          req.flash("success", `${data.todoTitle} added.`);
-          res.redirect(`/lists/${data.listID}`);
-        } catch (err) {
-          next(err);
-        }
-      }
+      withAttemptAsync(async (req, res) => {
+        const { listID, todoTitle } = matchedData(req);
+        await res.locals.todoStore.addTodo(listID, todoTitle);
+        req.flash("success", `${todoTitle} added.`);
+        res.redirect(`/lists/${listID}`);
+      }),
     ];
   },
 
-  async removeList(req, res, next) {
-    try {
-      const data = matchedData(req);
-      const todoList = await res.locals.todoStore.removeList(data.listID);
-      if (todoList) {
-        req.flash("success", `${todoList.title} deleted.`);
-      } else {
-        req.flash(
-          "error",
-          "Unable to delete that list. Are you sure it hasn't already been deleted?"
-        );
-      }
+  removeList: withAttemptAsync(
+    async (req, res) => {
+      const todoList = await res.locals.todoStore.removeList(
+        matchedData(req).listID,
+        { throw: true }
+      );
+      req.flash("success", `${todoList.title} deleted.`);
       res.redirect("/lists");
-    } catch (err) {
-      next(err);
+    },
+    {
+      expectedErrCodes: {
+        [TodoPGStore.ERROR_CODE_INVALID_LIST_ID]:
+          "Unable to delete that list. Are you sure it hasn't already been deleted?",
+      },
+      flashExpectedErrs: true,
     }
-  },
+  ),
 
   renderEditListForm(_req, res) {
     res.render("edit-list", {
@@ -335,37 +335,27 @@ const list = {
  * `TodoList`.
  */
 const todo = {
-  async toggle(req, res, next) {
-    try {
-      const data = matchedData(req);
-      const todo = await res.locals.todoStore.toggleDone(
-        data.todoID,
-        data.listID
-      );
-      if (todo.done) {
-        req.flash("success", `"${todo.title}" marked done.`);
-      } else {
-        req.flash("success", `"${todo.title}" marked not done.`);
-      }
-      res.redirect(`/lists/${data.listID}`);
-    } catch (err) {
-      next(err);
+  toggle: withAttemptAsync(async (req, res) => {
+    const { todoID, listID } = matchedData(req);
+    const todo = await res.locals.todoStore.toggleDone(todoID, listID, {
+      throw: true,
+    });
+    if (todo.done) {
+      req.flash("success", `"${todo.title}" marked done.`);
+    } else {
+      req.flash("success", `"${todo.title}" marked not done.`);
     }
-  },
+    res.redirect(`/lists/${listID}`);
+  }),
 
-  async removeTodo(req, res, next) {
-    try {
-      const data = matchedData(req);
-      const todo = await res.locals.todoStore.removeTodo(
-        data.todoID,
-        data.listID
-      );
-      req.flash("success", `"${todo.title}" deleted.`);
-      res.redirect(`/lists/${data.listID}`);
-    } catch (err) {
-      next(err);
-    }
-  },
+  removeTodo: withAttemptAsync(async (req, res) => {
+    const { todoID, listID } = matchedData(req);
+    const todo = await res.locals.todoStore.removeTodo(todoID, listID, {
+      throw: true,
+    });
+    req.flash("success", `"${todo.title}" deleted.`);
+    res.redirect(`/lists/${listID}`);
+  }),
 };
 
 /**
@@ -394,49 +384,96 @@ app.use(session({
 app.use(flash());
 
 app.use((_req, res, next) => {
+  // Define any res.locals properties here, so a property doesn't seemingly
+  // show up out of nowhere in some other middleware.
   res.locals.todoStore = new TodoPGStore();
+  res.locals.todoList = null;
+  res.locals.requiresTransactionCommit = false;
+  res.locals.beginTransaction = async function(options) {
+    await this.todoStore.beginTransaction(options);
+    this.requiresTransactionCommit = true;
+  };
+  res.locals.commitTransaction = async function() {
+    if (this.requiresTransactionCommit) {
+      await this.todoStore.commitTransaction();
+    }
+    this.requiresTransactionCommit = false;
+  };
   next();
 });
 
-app.param("listID", async (req, _res, next) => {
-  await (createPathParamValidationChain(
-    "listID",
-    "list",
-    async (listID, { req }) => {
-      req.res.locals.todoList = await req.res.locals.todoStore.findList(
-        listID
-      );
-      return req.res.locals.todoList !== undefined;
-    }
-  )).run(req);
-
-  attachRequestValidationResult(req);
-  if (!req.validationResult.isEmpty()) {
-    next(new Error(req.validationResult.array()[0]));
-    return;
-  }
+app.use((_req, res, next) => {
+  // It's common for a middleware function _not_ to call next() once it's
+  // finished processing the request and has sent or begun sending a response.
+  // Therefore, it's difficult to define an operation you want to run after most
+  // (or all) successful responses (for example, committing a database
+  // transaction).
+  //
+  // One strategy would be forcing _all_ middleware to always call next() and
+  // continue delegating to any further matched routes, including the route
+  // where the "always run" code lives. However, this can cause other route
+  // paths to unexpectedly match and execute, as happens in
+  // `app.param("listID", ...)` below if the `/lists/new` middleware calls
+  // next(): the `/lists/new` route looks identical to a `/lists/:listID` route,
+  // so the param handler gets invoked unexpectedly.
+  //
+  // Another strategy would be to define multiple routers, where one or two of
+  // them have the exclusive responsibility to handle beginning, committing, and
+  // rolling back transactions. This would require the routers to share a number
+  // of routes in common, and depend on the app middleware to explicitly call
+  // next("router") when they've successfully processed a request.
+  //
+  // Instead, I'll register this event handler for each request/response pair
+  // to piggyback on the response's "finish" event. You can do this because
+  // express's objects build upon standard Node `http` objects, which can emit
+  // and respond to events:
+  // https://nodejs.org/api/http.html#class-httpserverresponse
+  res.once("finish", () => {
+    res.locals.commitTransaction();
+  });
   next();
 });
 
-app.param("todoID", async (req, _res, next) => {
-  await (createPathParamValidationChain(
-    "todoID",
-    "todo",
-    async (todoID, { req }) => {
-      const listID = matchedData(req).listID;
-      return (
-        (await req.res.locals.todoStore.findTodo(todoID, listID)) !==
-        undefined
-      );
-    }
-  )).run(req);
+// eslint-disable-next-line max-lines-per-function
+app.param("listID", async (req, res, next) => {
+  await createPathParamValidationChain("listID", "list").run(req);
 
-  attachRequestValidationResult(req);
-  if (!req.validationResult.isEmpty()) {
-    next(new Error(req.validationResult.array()[0]));
-    return;
-  }
-  next();
+  await withAttemptAsync(
+    async (req, res, next) => {
+      const readOnly = req.method !== "POST";
+      attachRequestValidationResult({ throw: true })(req);
+      await res.locals.beginTransaction({ readOnly });
+      res.locals.requiresTransactionCommit = true;
+      res.locals.todoList = await res.locals.todoStore.findList(
+        req.params.listID,
+        { throw: true }
+      );
+      next();
+    },
+    {
+      expectedErrCodes: {
+        [TodoPGStore.ERROR_CODE_INVALID_LIST_ID]: "That list doesn't exist.",
+      },
+    }
+  )(req, res, next);
+});
+
+app.param("todoID", async (req, res, next) => {
+  await createPathParamValidationChain("todoID", "todo").run(req);
+
+  await withAttemptAsync(
+    async (req, _res, next) => {
+      attachRequestValidationResult({ throw: true })(req);
+      const { listID, todoID } = matchedData(req);
+      await req.res.locals.todoStore.findTodo(todoID, listID, { throw: true });
+      next();
+    },
+    {
+      expectedErrCodes: {
+        [TodoPGStore.ERROR_CODE_INVALID_TODO_ID]: "That todo doesn't exist.",
+      },
+    }
+  )(req, res, next);
 });
 
 app.map({
@@ -479,8 +516,22 @@ app.map({
 });
 
 // Register final error-generating middleware for all unused methods and paths
-app.all("*", (_req, _res, next) => {
-  next(new Error(`The route: ${_req.method} ${_req.path} doesn't exist`));
+app.all("*", (_req, res, next) => {
+  if (!res.headersSent) {
+    next(new Error(`The route: ${_req.method} ${_req.path} doesn't exist`));
+  }
+});
+
+app.use(async (err, _req, res, next) => {
+  try {
+    await res.locals.todoStore.rollbackTransaction();
+  } catch (newErr) {
+    console.log(new Error(
+      "Caught new error attempting to rollback transaction",
+      { cause: newErr }
+    ));
+  }
+  next(err);
 });
 
 app.use((err, _req, res, _next) => {
