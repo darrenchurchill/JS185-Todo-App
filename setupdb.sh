8d63762f18ef98af37aa1fee1eb53551444479ad
Darren Churchill <darrenmchurchill@gmail.com>
2024-11-02 14:30:39 -0700

    Add optional users.sql statements to setupdb.sh
    Although user.sql doesn't contain plaintext passwords, you probably
    still don't want to check it into git.

--- setupdb.sh
+++ setupdb.sh
@@ -4,10 +4,17 @@
 # - drop and re-create the todo-lists database
 # - create the necessary tables from sql/schema.sql
 # - insert the seed data from sql/seed-data.sql
+# - insert the seed data from sql/users.sql, if it exists
 
 DATABASE="todo-lists"
+USERS="sql/users.sql"
 
 dropdb "$DATABASE"
 createdb "$DATABASE"
 psql "$DATABASE" < sql/schema.sql
 psql "$DATABASE" < sql/seed-data.sql
+if [ -f "$USERS" ]; then
+  psql "$DATABASE" < "$USERS"
+else
+  echo "No $USERS file. Skipping users table INSERTS"
+fi
